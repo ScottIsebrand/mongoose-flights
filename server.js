308de1dad3e3876cb040809cc2877cc3b0c457e1
Scott Isebrand <scottisebrand@gmail.com>
2023-04-17 10:29:50 -0400

    Completed lab.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.set('view engine', 'jsx');
 app.engine('jsx', require('jsx-view-engine').createEngine());
 
-// === MIDDLEWARE (app.use)
+// === MIDDLEWARE (app.use); parsess the data from the request; makes data accessible
 app.use(express.urlencoded({ extended: false }));
 // app.use((req, res, next) => {
 //   console.log(req.url);
@@ -24,14 +24,14 @@ app.get('/', (req, res) => {
   res.send('<h1>Welcome to a world of flights.</h1>');
 });
 
-// Route: INDEX (ie HTTP verb is GET; action is index; Mongoose func. is .find)
+// Route: INDEX (ie HTTP verb is GET; action is index; Mongoose func. is .find; CRUD op is Read)
 app.get('/flights', (req, res) => {
   Flight.find({}, (error, allFlights) => {
     res.render('Index', { flights: allFlights });
   });
 });
 
-// Route: POST flight/document (ie HTTP verb is POST; action is create; Mongoose func. is .create)
+// Route: POST flight/document (ie HTTP verb is POST; action is create; Mongoose func. is .create; CRUD op is Create)
 app.post('/flights', (req, res) => {
   Flight.create(req.body, (error, createdFlight) => {
     res.redirect('/flights');
